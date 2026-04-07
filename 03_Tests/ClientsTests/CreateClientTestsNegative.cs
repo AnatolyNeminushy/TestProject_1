1923884b987066e919c36130192440a350e152ea
Anatoly Neminushy <todin5005@gmail.com>
2026-04-07 11:28:20 +0300

    refactor: изменил название классов

---
+++ 03_Tests/ClientsTests/CreateClientTestsNegative.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using System.Text.Json;
+
+namespace Clients;
+using BaseSpaceRequest;
+
+public class CreateClientTestsNegative
+{
+    
+    private RequestsClients Request = new RequestsClients();
+   
+
+    [Fact]
+    public async Task CreateNewClientTest()
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+        
+
+        // Asserts
+        // 
+        // Негативный сценарий
+        // Create
+        var responseNegativeCreate = await Request.RequestForEndpointNegativeCreateClients(); 
+        Console.WriteLine($"CreateNegative-StatusCode:{responseNegativeCreate.StatusCode}");
+       
+        Assert.Equal(HttpStatusCode.BadRequest, responseNegativeCreate.StatusCode);
+
+    }
+}
