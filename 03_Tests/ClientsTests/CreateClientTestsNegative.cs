95aed0bf80931c13415b7532043af926deb955ef
Anatoly Neminushy <todin5005@gmail.com>
2026-04-08 17:35:56 +0300

    refactor: структурировал тесты, использовал перегрузку метода CreateRequestForEndpointClients

--- 03_Tests/ClientsTests/CreateClientTestsNegative.cs
+++ 03_Tests/ClientsTests/CreateClientTestsNegative.cs
@@ -2,31 +2,44 @@ using System.Net;
 using System.Text.Json;
 
 namespace Clients;
+
 using BaseSpaceRequest;
 
 public class CreateClientTestsNegative
 {
-    
-    private RequestsClients Request = new RequestsClients();
-   
+    private RequestsClients requestsClients = new RequestsClients();
 
     [Fact]
     public async Task CreateNewClientTest()
     {
-        var options = new JsonSerializerOptions
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        // Act
+        var registeredDataClient = new DataClients
         {
-            PropertyNameCaseInsensitive = true,
+            PhoneNumber = "+79788902369",
+            Login = "DimaFire",
+            Email = "dmitrytkachenko@yandex.ru",
+            FirstName = "Дмитрий",
+            LastName = "Ткаченко",
+            MiddleName = "Андреевич",
+            Sex = "Male",
+            Address = "ул. Ленина, 45",
+            Birthdate = "1998-12-06",
+            Password = "Dima5678",
         };
-        
-
         // Asserts
-        // 
+        //
         // Негативный сценарий
         // Create
-        var responseNegativeCreate = await Request.RequestForEndpointNegativeCreateClients(); 
-        Console.WriteLine($"CreateNegative-StatusCode:{responseNegativeCreate.StatusCode}");
-       
-        Assert.Equal(HttpStatusCode.BadRequest, responseNegativeCreate.StatusCode);
+        var (responseNegativeCreateClient, dataClients) =
+            await requestsClients.CreateRequestForEndpointClients(registeredDataClient);
+        // var responseNegativeCreateClient = await requestsClients.CreateRequestForEndpointClients(
+        //     registeredDataClient
+        // );
+        Console.WriteLine(
+            $"[NEGATIVE_CREATE] StatusCode:{responseNegativeCreateClient.StatusCode}"
+        );
 
+        Assert.Equal(HttpStatusCode.BadRequest, responseNegativeCreateClient.StatusCode);
     }
 }
