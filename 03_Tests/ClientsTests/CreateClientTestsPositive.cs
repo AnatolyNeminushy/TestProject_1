770e2d1935d9a7795f642eea0740448b318193d4
Anatoly Neminushy <todin5005@gmail.com>
2026-04-08 13:12:47 +0300

    refactor: структурировал тесты по паттерну ААА, сделал тест независимым

--- 03_Tests/ClientsTests/CreateClientTestsPositive.cs
+++ 03_Tests/ClientsTests/CreateClientTestsPositive.cs
@@ -2,74 +2,61 @@ using System.Net;
 using System.Text.Json;
 
 namespace Clients;
+
 using BaseSpaceRequest;
 
 public class CreateClientTestsPositive
 {
-    
-    private RequestsClients Request = new RequestsClients();
-
-    
-   
+    private RequestsClients requestClients = new RequestsClients();
+    private AuthenticationToken postRequestToken = new AuthenticationToken();
 
     [Fact]
     public async Task CreateNewClientTest()
     {
-        var options = new JsonSerializerOptions
-        {
-            PropertyNameCaseInsensitive = true,
-        };
-        
+        // Act
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var (responseCreateClient, dataClients) =
+            await requestClients.CreateRequestForEndpointClients();
+        Assert.NotNull(responseCreateClient.Content);
 
+        var login = dataClients.Login;
+        var password = dataClients.Password;
+        Assert.NotNull(login);
+        Assert.NotNull(password);
+
+        var autToken = await postRequestToken.RequestToObtainAuthenticationToken(login, password);
+        Assert.NotNull(autToken.Content);
+        var dataAuthTokenDeserialize = JsonSerializer.Deserialize<DataClients>(
+            autToken.Content,
+            options
+        );
+        Assert.NotNull(dataAuthTokenDeserialize);
+        var accessToken = dataAuthTokenDeserialize.AccessToken;
+
+        // Arrange
+        Assert.NotNull(accessToken);
+        var responseGetClient = await requestClients.GetRequestForEndpointClients(accessToken);
+        Assert.NotNull(responseGetClient.Content);
+        var getDataAfterCreateClientDeserialize = JsonSerializer.Deserialize<DataClients>(
+            responseGetClient.Content,
+            options
+        );
+        Assert.NotNull(getDataAfterCreateClientDeserialize);
+        // Проверил, что лежит внутри getDeserialize
+        // Console.WriteLine(getDeserialize.Login);
+        // Console.WriteLine(getDeserialize.Email);
+        //
         // Asserts
-        // 
-        // Позитивный сценарий
-        // Create
-        var responseCreate = await Request.RequestForEndpointCreateClients(); 
-        Console.WriteLine($"Create-StatusCode:{responseCreate.StatusCode}");
-        
-        Assert.Equal(HttpStatusCode.OK, responseCreate.StatusCode);
-        Assert.NotNull(Request.serializationDataClients);
-        Console.WriteLine($"Serialization-Login:{Request.serializationDataClients.MiddleName}");
-       
+        Console.WriteLine($"[CREATE] Status Code:{responseCreateClient.StatusCode}");
+        Console.WriteLine($"[AUTH_TOKEN] Status Code:{autToken.StatusCode}");
+        Console.WriteLine($"[GET_CLIENT] Status Code:{responseGetClient.StatusCode}");
+        Console.WriteLine($"[CONTENT_GET_CLIENT] Status Code:{responseGetClient.Content}");
 
-        // Authentification
-        var responseToken = await Request.PostAuthentificationToken();
-        Console.WriteLine($"Authentification Content:{responseToken.Content}");
-        Assert.NotNull(responseToken.Content);
-        Assert.NotEmpty(responseToken.Content);
+        Assert.Equal(HttpStatusCode.OK, responseCreateClient.StatusCode);
+        Assert.Equal(HttpStatusCode.OK, autToken.StatusCode);
+        Assert.Equal(HttpStatusCode.OK, responseGetClient.StatusCode);
 
-    
-        Assert.NotNull(Request.serializationDataClients.AccessToken);
-        // Console.WriteLine($"Access Token:{Request.serializationDataClients.AccessToken}");
-
-
-
-        // Get
-        var responseGet = await Request.RequestForEndpointGetClients();
-        Console.WriteLine($"Http-StatusCode Get-request:{responseGet.StatusCode}");
-        Console.WriteLine($"Content-get:{responseGet.Content}");
-        Console.WriteLine($"Content-getSer:{Request.serializationDataClients.Login},{Request.serializationDataClients.MiddleName}");
-        Assert.Equal(HttpStatusCode.OK, responseGet.StatusCode);
-        Assert.NotNull(responseGet.Content);
-        Assert.NotEmpty(responseGet.Content);
-
-        var clientDataAfterGetRequest = JsonSerializer.Deserialize<DataClients>(responseGet.Content, options);
-        // Assert.Equal(Request.serializationDataClients, clientDataAfterGetRequest);
-        Assert.NotNull(clientDataAfterGetRequest);
-        Console.WriteLine($"Client-Get:{clientDataAfterGetRequest.Login}, {clientDataAfterGetRequest.Email},{clientDataAfterGetRequest.Password},{Request.serializationDataClients.Password}");
-        Console.WriteLine($"Client-Serialize:{Request.serializationDataClients.Login}, {Request.serializationDataClients.Email}");
-        Assert.Equal(Request.serializationDataClients.Login, clientDataAfterGetRequest.Login);
-        Assert.Equal(Request.serializationDataClients.Email, clientDataAfterGetRequest.Email);
-
-
-
-       
-        
-
-        
-        
-
-
+        Assert.Equal(dataClients.Login, getDataAfterCreateClientDeserialize.Login);
+        Assert.Equal(dataClients.Email, getDataAfterCreateClientDeserialize.Email);
     }
 }
