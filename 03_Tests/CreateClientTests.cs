9c59583532b743fc85333f4ea584513c6f9caaa8
Anatoly Neminushy <todin5005@gmail.com>
2026-04-13 16:14:20 +0300

    feat: добавлены новые тесты

--- 03_Tests/CreateClientTests.cs
+++ 03_Tests/CreateClientTests.cs
@@ -11,16 +11,12 @@ namespace Clients;
 public class CreateClientTests
 {
     private RestClients restClients = new RestClients();
-    private AuthenticationClient authenticationClient =
-        new AuthenticationClient();
+    private AuthenticationClient authenticationClient = new AuthenticationClient();
+    // TODO приватный метод создать
 
-    /// <summary>
-    /// Создание пользователя с валдиными данными.
-    /// </summary>
-    [Fact]
-    public async Task CreateNewClient_WithNewData_ReturnsOk()
+
+    private DataClients getDataClients(string nameParametr = null, string value = null)
     {
-        // Arrange
         var unique = DateTime.Now.Ticks;
         var random = new Random();
         var phoneNumber = "+7" + random.Next(100000000, 1000000000).ToString();
@@ -41,6 +37,31 @@ public class CreateClientTests
             Birthdate = formattedDate,
             Password = $"password{unique}",
         };
+        if (string.IsNullOrEmpty(nameParametr) || string.IsNullOrEmpty(value))
+        {
+            return data;
+        }
+        _ = nameParametr switch
+        {
+            "login" => data.Login = value,
+            "sex" => data.Sex = value,
+            "email" => data.Email = value,
+            "password" => data.Password = value,
+            "birthdate" => data.Birthdate = value,
+            _ => throw new ArgumentException("Неверный параметр")
+        };
+
+        return data;
+    }
+
+    /// <summary>
+    /// Создание пользователя с валидными данными.
+    /// </summary>
+    [Fact]
+    public async Task CreateNewClient_WithNewData_ReturnsOk()
+    {
+        // Arrange
+        var data = getDataClients();
 
         var createClientResponse = await restClients.CreateClient(data);
 
@@ -113,72 +134,112 @@ public class CreateClientTests
         string sex)
     {
         // Arrange
-        var titleError = "One or more validation errors occurred.";
-        var registeredDataClient = new DataClients
-        {
-            Login = "DimaFire",
-            Email = "dmitrytkachenko@yandex.ru",
-            PhoneNumber = "+79788902369",
-            FirstName = "Дмитрий",
-            LastName = "Ткаченко",
-            MiddleName = "Андреевич",
-            Sex = sex,
-            Address = "ул. Ленина, 45",
-            Birthdate = "1998-12-06",
-            Password = "Dima5678",
-        };
-
+        var data = getDataClients("sex", sex);
         // Act
         var сreateClientResponse =
-            await restClients.CreateClient(registeredDataClient);
-        var сreateClientData = JsonDeserializer
-            .DeserializeData<ErrorCreateClient>(сreateClientResponse.Content);
+            await restClients.CreateClient(data);
 
-        Console.WriteLine(сreateClientData.Title);
         // Asserts
         Assert.Equal(
-            HttpStatusCode.BadRequest, сreateClientResponse.StatusCode);
-        Assert.Equal(
-            titleError, сreateClientData.Title);
+            HttpStatusCode.UnprocessableContent, сreateClientResponse.StatusCode);
     }
 
     [Theory]
     [InlineData(null)]
+    [InlineData("")]
     [InlineData("1234")]
     [InlineData("dfdgdg")]
     [InlineData("@#$$#")]
-    [InlineData("")]
     public async Task CreateNewClient_WithInvalidEmail_ReturnsBadRequest(
         string email)
     {
         // Arrange
-        var titleError = "One or more validation errors occurred.";
-        var registeredDataClient = new DataClients
-        {
-            Login = "DimaFire",
-            Email = email,
-            PhoneNumber = "+79788902369",
-            FirstName = "Дмитрий",
-            LastName = "Ткаченко",
-            MiddleName = "Андреевич",
-            Sex = "Male",
-            Address = "ул. Ленина, 45",
-            Birthdate = "1998-12-06",
-            Password = "Dima5678",
-        };
+        var data = getDataClients("email", email);
 
         // Act
         var сreateClientResponse =
-            await restClients.CreateClient(registeredDataClient);
-        var сreateClientData = JsonDeserializer
-            .DeserializeData<ErrorCreateClient>(сreateClientResponse.Content);
+            await restClients.CreateClient(data);
 
-        Console.WriteLine(сreateClientData.Title);
         // Asserts
         Assert.Equal(
-            HttpStatusCode.BadRequest, сreateClientResponse.StatusCode);
+            HttpStatusCode.UnprocessableContent, сreateClientResponse.StatusCode);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public async Task CreateNewClient_WithInvalidLogin_ReturnsBadRequest(
+       string login)
+    {
+        // Arrange
+        var data = getDataClients("login", login);
+
+        // Act
+        var сreateClientResponse =
+            await restClients.CreateClient(data);
+
+        // Asserts
         Assert.Equal(
-            titleError, сreateClientData.Title);
+            HttpStatusCode.UnprocessableContent, сreateClientResponse.StatusCode);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("123456dfsd")]
+    [InlineData("fkkoe454ldlsl343lssl5")]
+    public async Task CreateNewClient_WithInvalidPassword_ReturnsBadRequest(
+       string password)
+    {
+        // Arrange
+        var data = getDataClients("password", password);
+
+        // Act
+        var сreateClientResponse =
+            await restClients.CreateClient(data);
+
+        // Asserts
+        Assert.Equal(
+            HttpStatusCode.UnprocessableContent, сreateClientResponse.StatusCode);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("2020-12-03")]
+    public async Task CreateNewClient_WithInvalidBirthDate_ReturnsBadRequest(
+   string birthdate)
+    {
+        // Arrange
+        var data = getDataClients("birthdate", birthdate);
+
+        // Act
+        var сreateClientResponse =
+            await restClients.CreateClient(data);
+
+        // Asserts
+        Assert.Equal(
+            HttpStatusCode.UnprocessableContent, сreateClientResponse.StatusCode);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("+67050-5")]
+    [InlineData("+798756778980")]
+    public async Task CreateNewClient_WithInvalidPhoneNumber_ReturnsBadRequest(
+   string phonenumber)
+    {
+        // Arrange
+        var data = getDataClients("phonenumber", phonenumber);
+
+        // Act
+        var сreateClientResponse =
+            await restClients.CreateClient(data);
+
+        // Asserts
+        Assert.Equal(
+            HttpStatusCode.UnprocessableContent, сreateClientResponse.StatusCode);
     }
 
 }
