36a91ed532d193c7cc16d27397d39fc4cb7d838e
Anatoly Neminushy <todin5005@gmail.com>
2026-05-07 10:17:11 +0300

    add: добавил тесты на получение информации о доступных продуктах.

---
+++ Tests/ShowcaseTests.cs
@@ -0,0 +1,48 @@
+using System.Net;
+
+namespace TestProjectIntern_n1.Tests;
+
+/// <summary>
+/// Тесты на получение доступных продуктов.
+/// </summary>
+public class ShowcaseTests : BaseTest
+{
+    /// <summary>
+    /// Получение информации о доступных продуктов.
+    /// </summary>
+    /// <returns></returns>
+    public async Task ShowcaseProducts_ReturnsOk()
+    {
+        // Act
+        var showcaseProductsResponse = await ShowcaseRestClient.GetShowcaseProducts();
+
+        // Asserts
+        Assert.Equal(HttpStatusCode.OK, showcaseProductsResponse.StatusCode);
+    }
+
+    /// <summary>
+    /// Получение информации о доступном продукте.
+    /// </summary>
+    /// <returns></returns>
+    public async Task ShowcaseProduct_WithProductId_ReturnsOk()
+    {
+        // Act
+        var showcaseProductsResponse = await ShowcaseRestClient.GetShowcaseProduct(1);
+
+        // Asserts
+        Assert.Equal(HttpStatusCode.OK, showcaseProductsResponse.StatusCode);
+    }
+
+    /// <summary>
+    /// Получение информации о доступном продукте c невалидным id.
+    /// </summary>
+    /// <returns></returns>
+    public async Task ShowcaseProduct_WithInvaildProductId_ReturnsOk()
+    {
+        // Act
+        var showcaseProductsResponse = await ShowcaseRestClient.GetShowcaseProduct(0);
+
+        // Asserts
+        Assert.Equal(HttpStatusCode.BadRequest, showcaseProductsResponse.StatusCode);
+    }
+}
